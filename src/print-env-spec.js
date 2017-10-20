e27af6db6f2c1943b5859285a0922cc0f64a24ed
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-20 11:33:47 -0400

    initial

---
+++ src/print-env-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const printEnv = require('.')
+
+describe('@bahmutov/print-env', () => {
+  it('write this test', () => {
+    console.assert(printEnv, 'should export something')
+  })
+})
