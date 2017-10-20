435acb3fbe305941dbe8ca2da351fd2d005e76b4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-20 11:37:18 -0400

    add bin

---
+++ bin/print-env.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const prefix = process.argv[2]
+if (!prefix) {
+  console.error('usage: print-env PREFIX')
+  console.error(
+    'finds and prints all environment variables that start with PREFIX'
+  )
+  console.error('like PREFIX_FOO=... PREFIX_BAR=...')
+  process.exit(-1)
+}
+
+const getVars = require('..')
+const variables = getVars(prefix)
+// an object
+variables.forEach(key => {
+  console.log('%s=%s', key, variables[key])
+})
