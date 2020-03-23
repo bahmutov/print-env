4cba3ed5be051a72f3e1b5e92ca46c07d35d26d9
Jules Amosah <teomik129@gmail.com>
2020-03-23 19:07:50 -0400

    feat(print-env): Add -e|--exists flag
    The print-env cli now supports an -e|--exists flag, which replaces the functionality of has-env; it prints environment variable names only.
    
    BREAKING CHANGE: has-env has been removed

--- __snapshots__/print-env.spec.js
+++ __snapshots__/print-env.spec.js
@@ -5,25 +5,40 @@ exports['@bahmutov/print-env returns only prefixed env vars 1'] = {
 }
 
 exports['@bahmutov/print-env supports multiple prefixes 1'] = {
-  "FOOXFF": "foo",
-  "FOOXFE": "bar",
-  "FOOXFA": "baz",
-  "BARXFF": "blanditiis",
+  "BARXFA": "enim",
   "BARXFE": "facere",
-  "BARXFA": "enim"
+  "BARXFF": "blanditiis",
+  "FOOXFA": "baz",
+  "FOOXFE": "bar",
+  "FOOXFF": "foo"
 }
 
-exports['@bahmutov/print-env cli has-env displays a message when no env variables are found 1'] = `
-No environment variables found
+exports['@bahmutov/print-env cli print-env displays usage on 0 args 1'] = `
+Usage: print-env [options] <PREFIX>...
+
+Finds and prints the names and values of all environment variables present that start with any of the PREFIXes 
+
+Options:
+  -e, --exists  Print only variable names
+  -h, --help    display help for command
 `
 
-exports['@bahmutov/print-env cli print-env prints sorted variables 1'] = `
+exports['@bahmutov/print-env cli print-env prints sorted variables with values 1'] = `
+Found environment variables:
 FOOXFA=baz
 FOOXFE=bar
 FOOXFF=foo
 `
 
+exports['@bahmutov/print-env cli print-env prints sorted variables without values when passed -e|--exists flag 1'] = `
+Found environment variables:
+FOOXFA
+FOOXFE
+FOOXFF
+`
+
 exports['@bahmutov/print-env cli print-env supports multiple prefixes 1'] = `
+Found environment variables:
 BARXFA=enim
 BARXFE=facere
 BARXFF=blanditiis
@@ -32,19 +47,6 @@ FOOXFE=bar
 FOOXFF=foo
 `
 
-exports['@bahmutov/print-env cli has-env prints present sorted variables 1'] = `
-Found environment variables:
-FOOXFA
-FOOXFE
-FOOXFF
-`
-
-exports['@bahmutov/print-env cli has-env supports multiple prefixes 1'] = `
-Found environment variables:
-BARXFA
-BARXFE
-BARXFF
-FOOXFA
-FOOXFE
-FOOXFF
+exports['@bahmutov/print-env cli print-env displays a message when no env variables are found 1'] = `
+No environment variables found
 `
